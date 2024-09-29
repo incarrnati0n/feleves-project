43381cd3fd7185bad1c4a9f1f126bc2e86657c74
incarrnati0n <horvathimi2001@gmail.com>
2024-09-29 16:51:18 +0200

    added working backend

--- backend/main.go
+++ backend/main.go
@@ -27,58 +27,47 @@ type GuestLog struct {
 
 var client *mongo.Client
 
-func main() {
-	fmt.Println("Hello world!")
-	connect()
-
+func main() {	
 	http.Handle("/", http.FileServer(http.Dir("../frontend/build")))
 	http.HandleFunc("/getAllDocuments", getAllDocuments)
+	http.HandleFunc("/writeDocument", write2Database)
 
-	fmt.Println("Server running on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Println("Server running on port 1337!")
+	log.Fatal(http.ListenAndServe(":1337", nil))
 }
 
 func initDb() {
-
-	t := time.Now()
-
-	formatted := fmt.Sprintf("%d-%02d-%02d", t.Year(), t.Month(), t.Day())
-
-	seedData := []interface{} {
-		GuestLog{Name: "Test Elek", Log: "Nagyon fasza, nagyon jó", Date: formatted},
-		GuestLog{Name: "Qu Key", Log: "ching ding, chong bong", Date: formatted},
-		GuestLog{Name: "L'e Copoá", Log: "Ca'va! Ca'va!", Date: formatted},
+	seedData := []interface{}{
+		GuestLog{Name: "asd", Log: "kek", Date: "2000-01-01"},
+		GuestLog{Name: "asd2", Log: "kek1", Date: "2000-01-01"},
+		GuestLog{Name: "asd3", Log: "kek2", Date: "2000-01-01"},
+		GuestLog{Name: "asd4", Log: "kek3", Date: "2000-01-01"},
 	}
 
 	collection := client.Database(dbName).Collection(collectionName)
-
 	_, err := collection.InsertMany(context.Background(), seedData)
-
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Println("Database successfully seeded!")
+	log.Println("Data seeded")
 }
 
-func connect() {
+func init() {
 	clientOptions := options.Client().ApplyURI(mongoURI)
 
 	var err error
-
-	client, err := mongo.Connect(context.Background(), clientOptions)
-
+	client, err = mongo.Connect(context.Background(), clientOptions)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	err = client.Ping(context.Background(), nil)
-
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Println("Connected to MongoDB database!")
+	log.Println("Connected to MongoDB!")
 
 	initDb()
 }
@@ -88,6 +77,11 @@ func getAllDocuments(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, GET")
 	w.Header().Set("Content-Type", "application/json")
+
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
 
 	collection := client.Database(dbName).Collection(collectionName)
 
@@ -110,6 +104,40 @@ func getAllDocuments(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Error encoding JSON", http.StatusInternalServerError)
 		return
 	}
+
 }
 
-func write2Database() {}
+func write2Database(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, POST")
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var newGuestLog GuestLog
+	err := json.NewDecoder(r.Body).Decode(&newGuestLog)
+	if err != nil {
+		http.Error(w, "Invalid JSON input", http.StatusBadRequest)
+		return
+	}
+
+	t := time.Now()
+
+	newGuestLog.Date = fmt.Sprintf("%d-%02d-%02d", t.Year(), t.Month(), t.Day())
+
+	collection := client.Database(dbName).Collection(collectionName)
+
+	_, err = collection.InsertOne(context.Background(), newGuestLog)
+
+	if err != nil {
+		http.Error(w, "Error inserting document", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintf(w, `{"message": "Guestlog successfully added"}`)
+
+}
