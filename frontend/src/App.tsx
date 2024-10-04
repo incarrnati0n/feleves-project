74fc397b05daea8dae3032a8c48d4a3d68ba188c
incarrnati0n <horvathimi2001@gmail.com>
2024-10-04 17:56:09 +0200

    created new page components and Bar.tsx done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,23 @@
 import React from "react";
-import "./App.css";
+import Bar from "./Bar";
+import About from "./pages/About";
+import Guestbook from "./pages/Guestbook";
+import Home from "./pages/Home";
+import Reserve from "./pages/Reserve";
+import { Route, Routes } from "react-router-dom";
 
 export default function App() {
-  return <div>Hello World!</div>;
+  return (
+    <>
+      <Bar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/reserve" element={<Reserve />} />
+          <Route path="/guestbook" element={<Guestbook />} />
+        </Routes>
+      </div>
+    </>
+  );
 }
--- frontend/src/Bar.tsx
+++ frontend/src/Bar.tsx
@@ -1,3 +1,36 @@
 import React from "react";
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
 
-export default function Bar() {}
+export default function Bar() {
+  return (
+    <nav className="nav">
+      <Link to="/" className="site-title">
+        Placeholder name
+      </Link>
+      <ul>
+        <CustomLink to="/about">About</CustomLink>
+        <CustomLink to="/reserve">Reserve</CustomLink>
+        <CustomLink to="/guestbook">Guestbook</CustomLink>
+      </ul>
+    </nav>
+  );
+}
+
+type Props = {
+  to: string;
+  children: React.ReactNode;
+  [key: string]: any;
+};
+
+function CustomLink({ to, children, ...props }: Props) {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  );
+}
