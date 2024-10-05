f40844585a3171df6d8946d82a1f427272e28fc1
incarrnati0n <horvathimi2001@gmail.com>
2024-10-05 15:37:21 +0200

    created LogPopup.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
-import React from "react";
+import { useState } from "react";
 import Bar from "./Bar";
+import LogPopup from "./LogPopup";
 
 export default function App() {
+  const [openPopup, setOpenPopup] = useState(false);
+
   return (
     <>
       <Bar />
+      <button onClick={() => setOpenPopup(true)}>Open me!</button>
+      <LogPopup openPopup={openPopup} setOpenPopup={setOpenPopup} />
     </>
   );
 }
--- frontend/src/LogPopup.tsx
+++ frontend/src/LogPopup.tsx
@@ -1,16 +1,62 @@
-import React from "react";
-import { Popup } from "reactjs-popup";
+import { useState } from "react";
+import axios from "axios";
+import Popup from "reactjs-popup";
 
-export default function LogPopup() {
+interface LogPopupProps {
+  openPopup: boolean;
+  setOpenPopup: (open: boolean) => void;
+}
+
+export default function LogPopup({ openPopup, setOpenPopup }: LogPopupProps) {
+  const [name, setName] = useState("");
+  const [log, setLog] = useState("");
+
+  const handleClosePopup = () => {
+    setName("");
+    setLog("");
+    setOpenPopup(false);
+  };
+
+  function createLog(name: string, log: string) {
+    const jsonPayload = {
+      name: name,
+      log: log,
+    };
+
+    axios.post("/createLog", jsonPayload).then((res) => {
+      if (res.status === 201) {
+        console.log("Successfully added log!");
+      } else {
+        console.log("Oh no something went wrong!");
+      }
+    });
+  }
+
   return (
-    <Popup>
-      <h3>Write some feedback!</h3>
+    <Popup
+      open={openPopup}
+      position={"center center"}
+      onClose={handleClosePopup}
+    >
+      <h3>Leave a log!</h3>
       <hr />
-      <label htmlFor="name">Enter your name:</label>
-      <input type="text" id="name" />
-      <label htmlFor="log">Enter your feedback:</label>
-      <input type="text" id="log" />
-      <input type="submit" id="submit" />
+      <label></label>
+      <input
+        type="text"
+        placeholder="Enter name..."
+        value={name}
+        id="name-input"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Enter log..."
+        value={log}
+        id="log-input"
+        onChange={(e) => setLog(e.target.value)}
+      />
+      <button onClick={() => createLog(name, log)}>Send Request</button>
+      <button onClick={handleClosePopup}>Cancel</button>
     </Popup>
   );
 }
